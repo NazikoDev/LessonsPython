57c9b250a5b17b10a7c59ef923eb9366d8fbd16a
Гулназик <gulnazikmahamadalieva@gmail.com>
2025-10-23 10:44:58 +0600

    дополгила э

--- homework/hw_6.py
+++ homework/hw_6.py
@@ -3,12 +3,12 @@ class Contact:
     id = 0
 
     def __init__(self, phone_number:str, name:str):
-        self.phone_number = phone_number
+        self.phone = phone_number
         self.name = name
         Contact.id += 1
 
     def __str__(self):
-        return f"name={self.name}; number{self.phone_number}"
+        return f"name={self.name}; number{self.phone}"
 
     @staticmethod
     def validate_phone_number(phone_number: str):
@@ -30,7 +30,8 @@ class ContactList:
     def add_contact(self, phone_number: str, name: str):
         try:
              Contact.validate_phone_number(phone_number)
-             ContactList.all_contacts[Contact.id] = str(Contact(phone_number, name))
+             contact = Contact(phone_number, name)
+             ContactList.all_contacts[Contact.id] = str(contact)
         except ValueError:
              return "Неверный номер тел"
 
@@ -41,18 +42,16 @@ class ContactList:
             print("такого контакта нет")
 
 
-# user1 = Contact("+996555555550", "nazik")
-# print(user1.validate_phone_number(user1.phone_number))
-#
-# contact_list = ContactList()
-# print(contact_list.add_contact("0777777777", "sendi"))
-# print(contact_list.add_contact("0555555555", "liza"))
-# print(contact_list.add_contact("0222222222", "lana"))
-# print(ContactList.all_contacts)
-# print(Contact.id)
-# contact_list.remove_contact(contact_id=1)
-# print(contact_list.add_contact("0333333333", "fil"))
-# print(Contact.id)
+
+contact_list = ContactList()
+print(contact_list.add_contact("0777777777", "sendi"))
+print(contact_list.add_contact("0555555555", "liza"))
+print(contact_list.add_contact("0222222222", "lana"))
+print(ContactList.all_contacts)
+print(Contact.id)
+contact_list.remove_contact(contact_id=1)
+print(contact_list.add_contact("0333333333", "fil"))
+print(Contact.id)
 
 
 
