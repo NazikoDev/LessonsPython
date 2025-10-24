ccfe92ef35b8eef9ea2d93e4f10945a746f99183
Гулназик <gulnazikmahamadalieva@gmail.com>
2025-10-24 17:47:43 +0600

    lesson7

--- lessons/lesson7.py
+++ lessons/lesson7.py
@@ -1,2 +1,16 @@
-import sqlite3
+import sqlite3      #запрос
+
+def create_tables():   #создаем таблицу
+    connetion.execute("""
+    CREATE TABLE students (
+         name TEXT,      #назввание колонки (name)  NOT NULL(ограничение нельзя оставлять не заполненой)
+         age INTEGER,    #INTEGER,(только цифры)
+         city TEXT,
+         )
+         """)
+
+if __name__ == "__main__":
+    connetion = sqlite3.connect("database.db")
+    create_tables()
+
 
